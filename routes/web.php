d5530252b77d8b02020e442481293c660b78050e
u.t <arcobaleno20180411@gmail.com>
2021-03-07 08:44:34 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/', 'MicropostsController@index');
+
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -24,6 +27,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+  Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+  Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]); 
 });
 
